c3ffcd1513c6a1b896cf99c27e3b13a80e822b84
Nicolas Raillot <nicolas.raillot@spottle.fr>
2019-08-07 13:29:38 +0200

    Binding posts vers ecran

--- components/PostItem.js
+++ components/PostItem.js
@@ -3,9 +3,13 @@ import { Text, View, StyleSheet } from 'react-native';
 
 export class PostItem extends Component {
     render() {
+
+        const { post } = this.props;
+console.log(this.props)
         return (
             <View style={styles.Container}>
-                <Text style={{textAlign: 'center'}}>Post Content.</Text>
+                <Text style={{textAlign: 'center'}}>{post.title}</Text>
+                <Text style={{textAlign: 'center'}}>{post.body}</Text>
             </View>
         );
     }
@@ -13,10 +17,12 @@ export class PostItem extends Component {
 
 const styles = StyleSheet.create({
     Container: {
+        flex: 0.1,
         borderRadius: 4,
         borderWidth: 1,
         borderColor: '#939497',
-        marginTop: 20,
+        margin: 10,
+        padding: 10,
         height:'10%',
         width:'90%',
         justifyContent: 'center',
--- components/Posts.js
+++ components/Posts.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import { Text, ScrollView, ActivityIndicator, View } from 'react-native';
 import {PostItem} from './PostItem';
+import {fetchPosts} from '../api';
 
 export class Posts extends Component {
+
+    state = {
+        posts: null
+    };
+
+    componentDidMount() {
+        fetchPosts().then(posts => {
+            this.setState({posts: posts.data});
+        })
+    }
+
     render() {
+        const { posts } = this.state;
+
         return (
-            <ScrollView contentContainerStyle={{ flex: 1, alignItems: "center" }}>
-                    <PostItem/>
-            </ScrollView>
+            <View style={{ flex: 1}}>
+                <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+                    {
+                        posts ?
+                            posts.map(post => {
+                               return <PostItem post={post}/>
+                            }) :  <ActivityIndicator style={{marginTop: 20}} size="large" />
+
+                    }
+                </ScrollView>
+            </View>
         );
     }
 }
